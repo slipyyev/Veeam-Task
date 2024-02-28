d92db3ede1f1ca8507c52bd20bd2c6e25806406c
Eugene Slipych <slipeug@ksu.edu>
2024-02-28 20:05:25 +0100

    Code cleanup, edgecase fixes, safer way of user input handling

--- FolderSync.cpp
+++ FolderSync.cpp
@@ -11,8 +11,8 @@
 
 namespace fs = std::filesystem; 
 
-#define ARGUMENTS_AMOUNT 5
-#define MICROSECONDS_IN_ONE_SECOND 1000000
+const int ARGUMENTS_AMOUNT = 5;
+const int MICROSECONDS_IN_ONE_SECOND = 1000000;
 
 const std::string ARGUMENTS_DESCRIPTION =
     "First argument — source file relative path\n"
@@ -20,7 +20,17 @@ const std::string ARGUMENTS_DESCRIPTION =
     "Third argument — log file relative path\n"
     "Fourth argument — synchronization interval in seconds\n";
 
-void syncFolders(const fs::path & sourcePath, const fs::path & replicaPath, const fs::path & logPath);
+const std::string SOURCE_DOESNT_EXIST = "Source directory didn't exist, one was created";
+const std::string REPLICA_DOESNT_EXIST = "Replica directory didn't exist, one was created";
+const std::string LOG_PATH_DOESNT_EXIST = "Log path didn't exist, one was created";
+const std::string LOG_FILE_DOESNT_EXIST = "Log file didn't exist, one was created";
+
+const std::string SOURCE_CREATING_ERROR = "An exception occured when creating a source folder: ";
+const std::string REPLICA_CREATING_ERROR = "An exception occured when creating a replica folder: ";
+const std::string LOG_PATH_CREATING_ERROR = "An exception occured when creating a logging folder: ";
+
+
+void syncFolders(const fs::path & sourcePath, const fs::path & replicaPath, std::stringstream & logStream);
 
 
 std::string calculateMD5(const std::string& filename) {
@@ -66,73 +76,103 @@ bool filesHaveSameHash(const fs::path & sourcePath, const fs::path & replicaPath
 }
 
 
-bool syncSameFiles(const fs::path & sourcePath, const fs::path & replicaPath, const fs::path & logPath, const std::set<fs::path> & sameFiles) {
+bool syncSameFiles(const fs::path & sourcePath, const fs::path & replicaPath, std::stringstream & logStream, const std::set<fs::path> & sameFiles) {
     
     
     for (const fs::path & f : sameFiles) {
         if (fs::is_directory(sourcePath / f)) {
-            syncFolders(sourcePath / f, replicaPath / f, logPath);
+            syncFolders(sourcePath / f, replicaPath / f, logStream);
         }
         else if (!filesHaveSameHash(sourcePath / f, replicaPath / f)) {
             fs::remove_all(replicaPath / f);
             fs::copy(sourcePath / f, replicaPath / f);
-            std::cout << "File " << f << " was updated from the replica folder" << std::endl;
+            logStream << "File " << f << " was updated in the replica folder" << std::endl;
+            return false;
         }
     }
 
     return true;
 }
 
-bool eraseDeletedFiles(const fs::path & replicaPath, const fs::path & logPath, const std::set<fs::path> & inReplicaOnly) {
+bool eraseDeletedFiles(const fs::path & replicaPath, std::stringstream & logStream, const std::set<fs::path> & inReplicaOnly) {
 
     for (const fs::path & f : inReplicaOnly) {
         fs::remove_all(replicaPath / f);
-        std::cout << "File " << f << " removed from the replica folder" << std::endl;
+        logStream << "File " << f << " was removed from the replica folder" << std::endl;
     }
 
     return true;
 }
 
-bool addNewlyCreatedFiles(const fs::path & sourcePath, const fs::path & replicaPath, const fs::path & logPath, const std::set<fs::path> & inSourceOnly) {
+bool addNewlyCreatedFiles(const fs::path & sourcePath, const fs::path & replicaPath, std::stringstream & logStream, const std::set<fs::path> & inSourceOnly) {
 
     for (const fs::path & f : inSourceOnly) {
         fs::copy(sourcePath / f, replicaPath / f, fs::copy_options::recursive);
-        std::cout << "File " << f << " added to the replica folder" << std::endl;
+        logStream << "File " << f << " was added to the replica folder" << std::endl;
     }
 
     return true;
 }
 
 
+bool checkIfAllFoldersExist(const fs::path & sourcePath, const fs::path & replicaPath, const fs::path & logPath, std::stringstream & logStream) {
+    
+    if (!fs::exists(sourcePath)) {
+        try {
+            fs::create_directories(sourcePath);
+            logStream << SOURCE_DOESNT_EXIST << std::endl;
 
-void syncFolders(const fs::path & sourcePath, const fs::path & replicaPath, const fs::path & logPath) {
+        } catch (const std::exception& e) {
+            logStream << SOURCE_CREATING_ERROR << e.what() << std::endl;
+            return false;
+        }
+    }
+
+    
+    if (!fs::exists(replicaPath)) {
+        try {
+            fs::create_directories(replicaPath);
+            logStream << REPLICA_DOESNT_EXIST << std::endl;
+
+        } catch (const std::exception& e) {
+            logStream << REPLICA_CREATING_ERROR << e.what() << std::endl;
+            return false;
+        }
+    }
+
+    if (!fs::exists(logPath.parent_path())) {
+        try {
+            fs::create_directories(logPath.parent_path());
+            logStream << LOG_PATH_DOESNT_EXIST << std::endl;
+
+        } catch (const std::exception& e) {
+            logStream << LOG_PATH_CREATING_ERROR << e.what() << std::endl;
+            return false;
+        }
+    }
+
+    std::ifstream logFileTest (logPath);
+
+    if (logFileTest.is_open()) {
+        logFileTest.close();
+    }
+    else {
+        logStream << LOG_FILE_DOESNT_EXIST << std::endl;
+    }
+
+    return true;
+}
+
+void syncFolders(const fs::path & sourcePath, const fs::path & replicaPath, std::stringstream & logStream) {
     
     std::set<fs::path> filesInSource;
     std::set<fs::path> filesInReplica;
-
 
     for(const fs::directory_entry & dirEntry: fs::directory_iterator(sourcePath))
         filesInSource.insert(dirEntry.path().filename());
 
     for(const fs::directory_entry & dirEntry: fs::directory_iterator(replicaPath))
         filesInReplica.insert(dirEntry.path().filename());
-
-    std::cout << "-------------------------------" << std::endl;
-    std::cout << "Files in source:" << std::endl;
-
-    for (auto p : filesInSource) {
-                std::cout << p << std::endl;
-    }
-
-
-    std::cout << "-------------------------------" << std::endl;
-    std::cout << "Files in replica:" << std::endl;
-
-    for (auto p : filesInReplica) {
-                std::cout << p << std::endl;
-    }
-
-    std::cout << "-------------------------------" << std::endl;
 
     std::set<fs::path> sameFiles;
 
@@ -156,61 +196,103 @@ void syncFolders(const fs::path & sourcePath, const fs::path & replicaPath, cons
         sameFiles.begin(), sameFiles.end(), 
         std::inserter(inReplicaOnly, inReplicaOnly.begin())
     );
-    
 
-    for (const auto & a : sameFiles ) {
-        std::cout << "Same file: " << a << std::endl;
-    }
-
-    syncSameFiles(sourcePath, replicaPath, logPath, sameFiles);
-    eraseDeletedFiles(replicaPath, logPath, inReplicaOnly);
-    addNewlyCreatedFiles(sourcePath, replicaPath, logPath, inSourceOnly);
+    syncSameFiles(sourcePath, replicaPath, logStream, sameFiles);
+    eraseDeletedFiles(replicaPath, logStream, inReplicaOnly);
+    addNewlyCreatedFiles(sourcePath, replicaPath, logStream, inSourceOnly);
 }
 
-
-void runSynchronisationLoop(const fs::path & sourcePath, const fs::path & replicaPath, const fs::path & logPath, int period) {
-
-    if (!fs::exists(sourcePath)) {
-        std::cout << "Source directory didn't exist, one was created" << std::endl;
-        fs::create_directory(sourcePath);
-    }
-    
-    if (!fs::exists(replicaPath)) {
-        std::cout << "Source directory didn't exist, one was created" << std::endl;
-        fs::create_directory(replicaPath);
-    }
-
-    if (!fs::exists(logPath.parent_path())) {
-        std::cout << "Log path didn't exist, one was created" << std::endl;
-        fs::create_directory(logPath.parent_path());
-    }
-
-    std::ifstream logFileTest (logPath);
-
-    if (logFileTest.is_open()) {
-        logFileTest.close();
-    }
-    else {
-        std::cout << "Log file didn't exist, one was created" << std::endl;
-    }
-
-    std::ofstream outfile;
-    outfile.open (logPath, std::ofstream::out | std::ofstream::app);
+std::string getTimestamp() {
 
     std::time_t currentTime = std::time(nullptr);
     std::string timeString;
+    timeString = std::ctime(&currentTime);
+    // Erasing the newline at the end of string
+    timeString.erase(timeString.length() - 1);
+
+    return "[" + timeString + "]";
+}
+
+void addReportToLogAndConsole(std::stringstream & logStream, const std::string & startTimestamp, const fs::path & logPath) {
+
+    if (logStream.str().empty())
+        return;
+
+    std::string content = logStream.str();
+    logStream.str(std::string());
+    logStream << startTimestamp << " Synchronization iteration has started" << std::endl;
+    logStream << content;
+    logStream << getTimestamp() << " Synchronization iteration has ended" << std::endl;
+    logStream << "-------------------------------------------------------------------" << std::endl;
+
+    std::ofstream file;
+    file.open (logPath, std::ofstream::out | std::ofstream::app);
+
+    if (!file) {
+        logStream << "Didn't manage to write to the log file \"" << logPath.filename() << "\"" << std::endl;
+    }
+    else {
+        std::cout << logStream.str();
+        file << logStream.str();
+    }
+
+    file.close();
+    // clear the stringstream
+    logStream.str(std::string());
+}
+
+bool runSynchronisationLoop(const fs::path & sourcePath, const fs::path & replicaPath, const fs::path & logPath, int period) {
+
+    std::stringstream logStream;
+    std::string syncStartTimestamp;
 
     while (true) {
 
-        timeString = std::ctime(&currentTime);
-        // Erasing the newline at the end of string
-        timeString.erase(timeString.length() - 1);
-        std::cout << "[" << timeString << "] Sync happens" << std::endl;
-
-        syncFolders(sourcePath, replicaPath, logPath);
-
+        syncStartTimestamp = getTimestamp();
+        if (!checkIfAllFoldersExist(sourcePath, replicaPath, logPath, logStream)) {
+            addReportToLogAndConsole(logStream, syncStartTimestamp, logPath);
+            return false;    
+        }
+        syncFolders(sourcePath, replicaPath, logStream);
+        addReportToLogAndConsole(logStream, syncStartTimestamp, logPath);
         usleep(period * MICROSECONDS_IN_ONE_SECOND);
     }
+
+    return true;
+}
+
+bool validateUserInput(const fs::path & sourcePath, const fs::path & replicaPath, const fs::path & logPath, int & period, char * argv[]) {
+    
+    if (sourcePath.empty() || sourcePath != argv[1]) {
+        std::cerr << "Source path is wrong" << std::endl;
+        return false;
+    }
+
+    if (replicaPath.empty() || replicaPath != argv[2]) {
+        std::cerr << "Replica path is wrong" << std::endl;
+        return false;
+    }
+
+    if (logPath.empty() || logPath.filename().empty()  || logPath != argv[3]) {
+        std::cerr << "Log path is wrong" << std::endl;
+        return false;
+    }
+
+    try {
+        period = std::stoi(argv[4]);
+    }
+    catch(const std::exception& e) {
+        std::cerr << "Synchronization interval is wrong" << std::endl;
+        std::cerr << ARGUMENTS_DESCRIPTION << std::endl;
+        return false;
+    }
+
+    if (period <= 0) {
+        std::cerr << "Synchronization interval is wrong" << std::endl;
+        return false;
+    }
+
+    return true;
 }
 
 
@@ -218,13 +300,9 @@ void runSynchronisationLoop(const fs::path & sourcePath, const fs::path & replic
 int main(int argc, char* argv[]) {
 
     if (argc != ARGUMENTS_AMOUNT) {
-        std::cout << "Number of arguments provided: " << argc << ", agruments needed: " << ARGUMENTS_AMOUNT << std::endl;
-        std::cout << ARGUMENTS_DESCRIPTION << std::endl;
+        std::cerr << "Number of arguments provided: " << argc << ", agruments needed: " << ARGUMENTS_AMOUNT << std::endl;
+        std::cerr << ARGUMENTS_DESCRIPTION << std::endl;
         return 1;
-    }
-
-    for (int i = 0; i < argc; ++i) {
-        std::cout << "argv[" << i << "] = " << argv[i] << std::endl;
     }
 
     int period;
@@ -233,37 +311,13 @@ int main(int argc, char* argv[]) {
     fs::path replicaPath = argv[2]; 
     fs::path logPath = argv[3];
 
-    if (!sourcePath.is_relative() || sourcePath.empty() || sourcePath != argv[1]) {
-        std::cerr << "Source path is wrong" << '\n';
+    if (!validateUserInput(sourcePath, replicaPath, logPath, period, argv)) {
         return 1;
     }
 
-    if (!replicaPath.is_relative() || replicaPath.empty() || replicaPath != argv[2]) {
-        std::cerr << "Replica path is wrong" << '\n';
+    if (!runSynchronisationLoop(sourcePath, replicaPath, logPath, period)) {
         return 1;
     }
-
-    if (!logPath.is_relative() || logPath.empty() || logPath != argv[3]) {
-        std::cerr << "Log path is wrong" << '\n';
-        return 1;
-    }
-
-    try {
-        period = std::stoi(argv[4]);
-    }
-    catch(const std::exception& e) {
-        std::cerr << "Synchronization interval is wrong" << '\n';
-        std::cout << ARGUMENTS_DESCRIPTION << std::endl;
-        return 1;
-    }
-
-    if (period <= 0) {
-        std::cerr << "Synchronization interval is wrong" << '\n';
-    }
-
-    std::cout << sourcePath << " " << replicaPath << " " << logPath << " " << period << std::endl;
-
-    runSynchronisationLoop(sourcePath, replicaPath, logPath, period);
 
     return 0;
 }
